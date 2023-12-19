90b58543dbf735e780edbba97c67773028eb9506
Alexanderzxc <yaaa.22@uni-dubna.ru>
2023-12-19 20:00:59 +0300

    new dz

---
+++ JS светофор/index.js
@@ -0,0 +1,10 @@
+﻿let lights = document.querySelectorAll('.light')
+let currentLight = 0;
+
+function toggleLights() {
+    lights[currentLight].classList.remove('active')
+    
+    currentLight = (currentLight + 1) % lights.length;
+
+    lights[currentLight].classList.add('active');
+}
